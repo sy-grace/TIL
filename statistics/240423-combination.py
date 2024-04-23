20d56fc7b72f66c283e4f713cd4633ec0d39b1f5
sy-grace <sygrace126@gmail.com>
2024-04-23 15:21:53 +0900

    feat: New Folder 'statistics'
    새 폴더 statistics 생성

---
+++ statistics/240423-combination.py
@@ -0,0 +1,4 @@
+import itertools
+cards = [1, 2, 3, 4]
+a = list(itertools.combinations(cards, 2))
+print(a)
---
+++ statistics/240423-permutation.py
@@ -0,0 +1,5 @@
+import itertools
+
+cards = [1, 2, 3, 4]
+a = list(itertools.permutations(cards, 2))
+print(a)
